67219275bab201dcda8d7eebd9a2d995d12458d5
hugues <ghugues@netc.fr>
2023-07-14 11:54:23 +0200

    Add support of JWT in auth

--- authentification.go
+++ authentification.go
@@ -1,7 +1,13 @@
 package airazor
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/base64"
+	"encoding/json"
+	"hash"
+	"time"
 )
 
 // One of the following fields
@@ -21,13 +27,14 @@ type AuthorizationJWT struct {
 	Jose map[string]any
 	Body map[string]any
 
-	// HS256
-	// HS384
-	// HS512
+	// Possibles values: HS256|HS384|HS512
+	// Else value will produce a none algo signature
+	Algo string
+	Key  []byte
 }
 
 // Generate the string to be used in HTTP headers. If the authentification is not defined,
-func (auth *Authorization) Header(previous string) string {
+func (auth *Authorization) Header(previous string, now time.Time) string {
 	if auth.None {
 		return ""
 	}
@@ -41,6 +48,73 @@ func (auth *Authorization) Header(previous string) string {
 	if raw := auth.Raw; raw != "" {
 		return raw
 	}
+	if jwt := auth.JWT; jwt != nil {
+		algo := jwtCanonizeAlgo(jwt.Algo)
+		joseB, _ := json.Marshal(mergeMapAny(
+			map[string]any{
+				"alg": algo,
+				"typ": "JWT",
+			},
+			jwt.Jose,
+		))
+		jose := base64.RawURLEncoding.EncodeToString(joseB)
+
+		bodyB, _ := json.Marshal(mergeMapAny(
+			map[string]any{
+				"iat": now.Unix(),
+				"exp": now.Add(time.Minute).Unix(),
+			},
+			jwt.Body,
+		))
+		body := base64.RawURLEncoding.EncodeToString(bodyB)
+
+		signature := ""
+		switch algo {
+		case "none":
+		case "HS256":
+			signature = hamacAndBase64(sha256.New, jwt.Key, jose, ".", body)
+		case "HS384":
+			signature = hamacAndBase64(sha512.New384, jwt.Key, jose, ".", body)
+		case "HS512":
+			signature = hamacAndBase64(sha512.New, jwt.Key, jose, ".", body)
+		}
+
+		return "Bearer " + jose + "." + body + "." + signature
+	}
 
 	return previous
+}
+
+// Merge the 2 map in a new one.
+// The a values overwrite b values.
+func mergeMapAny(a, b map[string]any) (r map[string]any) {
+	r = make(map[string]any, len(a)+len(b))
+	for k, v := range b {
+		r[k] = v
+	}
+	for k, v := range a {
+		r[k] = v
+	}
+	return
+}
+
+func jwtCanonizeAlgo(algo string) string {
+	switch algo {
+	case "HS256":
+		return "HS256"
+	case "HS384":
+		return "HS384"
+	case "HS512":
+		return "HS512"
+	default:
+		return "none"
+	}
+}
+
+func hamacAndBase64(h func() hash.Hash, key []byte, datas ...string) string {
+	hasher := hmac.New(h, key)
+	for _, d := range datas {
+		hasher.Write([]byte(d))
+	}
+	return base64.RawURLEncoding.EncodeToString(hasher.Sum(nil))
 }
--- authentification_test.go
+++ authentification_test.go
@@ -2,24 +2,66 @@ package airazor
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func TestAuthentificationEmpty(t *testing.T) {
+	checkAuth(t, "foo", Authorization{})
+}
 func TestAuthentificationNone(t *testing.T) {
-	assert.Equal(t, "", (&Authorization{None: true}).Header("foo"))
+	checkAuth(t, "", Authorization{None: true})
 }
 func TestAuthentificationBasic(t *testing.T) {
-	assert.Equal(t, "Basic YWxhZGRpbjpzZXNhbWVPdXZyZVRvaQ==",
-		(&Authorization{
-			Basic: &AuthorizationBasic{"aladdin", "sesameOuvreToi"},
-		}).Header("foo"))
+	checkAuth(t, "Basic YWxhZGRpbjpzZXNhbWVPdXZyZVRvaQ==", Authorization{
+		Basic: &AuthorizationBasic{"aladdin", "sesameOuvreToi"},
+	})
 }
 func TestAuthentificationBearer(t *testing.T) {
-	assert.Equal(t, "Bearer yolo",
-		(&Authorization{Bearer: "yolo"}).Header("foo"))
+	checkAuth(t, "Bearer yolo", Authorization{Bearer: "yolo"})
 }
 func TestAuthentificationRaw(t *testing.T) {
-	assert.Equal(t, "yolo",
-		(&Authorization{Raw: "yolo"}).Header("foo"))
+	checkAuth(t, "yolo", Authorization{Raw: "yolo"})
+}
+func TestAuthentificationJWTNone(t *testing.T) {
+	checkAuth(t, "Bearer eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJleHAiOjE2ODgxNjk2NjAsImlhdCI6MTY4ODE2OTYwMCwibmFtZSI6IkxhcmEgQ1JPRlQiLCJzdWIiOiJsYXJhQGNyb2Z0LmNvLnVrIn0.", Authorization{JWT: &AuthorizationJWT{
+		Body: map[string]any{
+			"sub":  "lara@croft.co.uk",
+			"name": "Lara CROFT",
+		},
+	}})
+}
+func TestAuthentificationJWTHS256(t *testing.T) {
+	checkAuth(t, "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODgxNjk2NjAsImlhdCI6MTY4ODE2OTYwMCwibmFtZSI6IkxhcmEgQ1JPRlQiLCJzdWIiOiJsYXJhQGNyb2Z0LmNvLnVrIn0.QUvM2IWxphicBD-Vw0dkIKxn4qQ1sUVYbwdsgrrnvEY", Authorization{JWT: &AuthorizationJWT{
+		Body: map[string]any{
+			"sub":  "lara@croft.co.uk",
+			"name": "Lara CROFT",
+		},
+		Algo: "HS256",
+		Key:  []byte("code"),
+	}})
+}
+func TestAuthentificationJWTHS384(t *testing.T) {
+	checkAuth(t, "Bearer eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODgxNjk2NjAsImlhdCI6MTY4ODE2OTYwMCwibmFtZSI6IkxhcmEgQ1JPRlQiLCJzdWIiOiJsYXJhQGNyb2Z0LmNvLnVrIn0.23aoUzlDPk7nuVaAAeI6kTRXxBW9NqJ8qu_EMltx1Tjkvf9yp0Q3FzMtPgPjFtbO", Authorization{JWT: &AuthorizationJWT{
+		Body: map[string]any{
+			"sub":  "lara@croft.co.uk",
+			"name": "Lara CROFT",
+		},
+		Algo: "HS384",
+		Key:  []byte("code"),
+	}})
+}
+func TestAuthentificationJWTHS512(t *testing.T) {
+	checkAuth(t, "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODgxNjk2NjAsImlhdCI6MTY4ODE2OTYwMCwibmFtZSI6IkxhcmEgQ1JPRlQiLCJzdWIiOiJsYXJhQGNyb2Z0LmNvLnVrIn0.O4BUqnCTA00Fu5HQ8dJzL-23PpsxHVe1wOc34RjDjQ7Nh953Oq5iVqHPkYv3zBt6WjUorCXiuUD9MWawJkxkQA", Authorization{JWT: &AuthorizationJWT{
+		Body: map[string]any{
+			"sub":  "lara@croft.co.uk",
+			"name": "Lara CROFT",
+		},
+		Algo: "HS512",
+		Key:  []byte("code"),
+	}})
+}
+func checkAuth(t *testing.T, header string, auth Authorization) {
+	assert.Equal(t, header, auth.Header("foo", time.Unix(1688169600, 0)))
 }
