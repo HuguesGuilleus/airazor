ae49d7c37cce83af2c16cdc4438ceb65c3be3499
hugues <ghugues@netc.fr>
2023-08-01 23:03:09 +0200

    ui-web: init

---
+++ ui-web/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	addr := flag.String("a", ":8000", "Listen address")
+	flag.Parse()
+
+	s := Server{}
+
+	log.Println("listen", *addr)
+	log.Fatal(http.ListenAndServe(*addr, &s))
+}
+
+var (
+	//go:embed index.html
+	indexHTML []byte
+
+	//go:embed style.css
+	styleCSS []byte
+
+	//go:embed index.js
+	indexJs []byte
+)
+
+type Server struct{}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		serveBytes(w, "text/html", indexHTML)
+	case "/index.js":
+		serveBytes(w, "application/javascript", indexJs)
+	case "/style.css":
+		serveBytes(w, "text/css", styleCSS)
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+func serveBytes(w http.ResponseWriter, mime string, content []byte) {
+	w.Header().Set("Content-Type", mime)
+	w.Header().Set("Content-Length", strconv.Itoa(len(content)))
+	w.Write(content)
+}
